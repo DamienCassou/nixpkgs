d8676fdd17752b86bff69e129e2150319ad02e3b
Damien Cassou <damien@cassou.me>
2020-08-12 06:19:19 +0200

    Mono 6.10

--- pkgs/development/compilers/mono/6.nix
+++ pkgs/development/compilers/mono/6.nix
@@ -1,9 +1,10 @@
-{ callPackage, Foundation, libobjc }:
+{ callPackage, Foundation, libobjc, python38 }:
 
 callPackage ./generic.nix ({
   inherit Foundation libobjc;
-  version = "6.0.0.313";
+  version = "6.10.0.104";
   srcArchiveSuffix = "tar.xz";
-  sha256 = "0l0cd6q5xh1vdm6zr78rkfqdsmrgzanjgpxvgig0pyd3glfyjim9";
+  sha256 = "0b1kgqqpk3kzlmzn2panh7jmkp43k196pml4hfnyzljjl9qfpmmq";
   enableParallelBuilding = true;
+  python = python38;
 })
